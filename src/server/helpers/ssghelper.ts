be68d6d442b9cab22a09967faed02fd5d166fea0
ozy <ozy@ozys-MacBook-Pro.local>
2023-09-19 16:34:31 +0530

    first production commit

---
+++ src/server/helpers/ssghelper.ts
@@ -0,0 +1,15 @@
+import { createServerSideHelpers } from '@trpc/react-query/server';
+
+import superjson from 'superjson';
+import { appRouter } from "~/server/api/root";
+import { db } from "~/server/db";
+
+
+
+export const generateServerHelper =()=> 
+createServerSideHelpers({
+ 
+    router: appRouter,
+    ctx: {db, userId: null},
+    transformer: superjson, // optional - adds superjson serialization
+  })
